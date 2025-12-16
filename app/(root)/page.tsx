98bd63c49de0fa78a9746a020925c2a1684bcb9e
salman <salmancrc9@gmail.com>
2025-12-16 15:14:13 +0500

    commented contributions section and nav item

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -209,7 +209,7 @@ export default function IndexPage() {
                     </p>
                 </div> */}
       </AnimatedSection>
-      <AnimatedSection
+      {/* <AnimatedSection
         direction="down"
         className="container space-y-6 bg-muted py-10 my-14"
         id="contributions"
@@ -239,7 +239,7 @@ export default function IndexPage() {
             </Button>
           </Link>
         </AnimatedText>
-      </AnimatedSection>
+      </AnimatedSection> */}
       <AnimatedSection
         direction="left"
         className="container space-y-6 py-10 my-14"
--- config/routes.ts
+++ config/routes.ts
@@ -12,10 +12,10 @@ export const routesConfig: any = {
       title: "Experience",
       href: "/experience",
     },
-    {
-      title: "Contributions",
-      href: "/contributions",
-    },
+    // {
+    //   title: "Contributions",
+    //   href: "/contributions",
+    // },
     {
       title: "Contact",
       href: "/contact",
