6457de61f4973100f1403244ca02f8ac7dabe294
salman <salmancrc9@gmail.com>
2025-12-27 17:08:55 +0500

    project link added in the project card

--- components/projects/project-card.tsx
+++ components/projects/project-card.tsx
@@ -5,6 +5,7 @@ import { Icons } from "@/components/common/icons";
 import { Button } from "@/components/ui/button";
 import ChipContainer from "@/components/ui/chip-container";
 import { ProjectInterface } from "@/config/projects";
+import CustomTooltip from "@/components/ui/custom-tooltip";
 
 interface ProjectCardProps {
   project: ProjectInterface;
@@ -13,9 +14,9 @@ interface ProjectCardProps {
 export default function ProjectCard({ project }: ProjectCardProps) {
   return (
     <div className="relative p-6 max-w-sm bg-background border border-border rounded-lg">
-      <div className="relative w-full h-[200px]">
+      <div className="relative w-full h-[200px] bg-muted flex items-center justify-center">
         <Image
-          className="rounded-lg border border-border object-cover"
+          className="rounded-lg border border-border object-contain"
           src={project.companyLogoImg}
           alt="img"
           fill
@@ -38,11 +39,18 @@ export default function ProjectCard({ project }: ProjectCardProps) {
           </Button>
         </Link>
       </div>
-      <div className="absolute bottom-4 right-4 p-3 rounded-full bg-background border border-border">
-        {project.type === "Personal" ? (
+      <div className="absolute bottom-4 right-4 p-3 rounded-full bg-background">
+        {/* {project.type === "Personal" ? (
           <Icons.userFill className="h-4 w-4" />
         ) : (
           <Icons.work className="h-4 w-4" />
+        )} */}
+        {project.websiteLink && (
+          <CustomTooltip text="Please note that some project links may be temporarily unavailable.">
+            <Link href={project.websiteLink} target="_blank">
+              <Icons.externalLink className="w-6 h-6 ml-4 text-muted-foreground hover:text-foreground " />
+            </Link>
+          </CustomTooltip>
         )}
       </div>
     </div>
